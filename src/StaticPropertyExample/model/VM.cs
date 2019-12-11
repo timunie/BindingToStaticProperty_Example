8f5337c3a7dc3aec62fa8f90117ed19881a7e957
timunie <47110241+timunie@users.noreply.github.com>
2019-12-11 09:56:11 +0100

    initial commit

---
+++ src/StaticPropertyExample/model/VM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticPropertyExample.model
+{
+    public class VM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public static event EventHandler<PropertyChangedEventArgs> StaticPropertyChanged;
+        
+
+        void RaisePropertyChanged (string PropertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        public static void RaiseStaticPropertyChanged(string PropertyName)
+        {
+            StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private int _Age;
+        public int Age
+        {
+            get { return _Age; }
+            set { _Age = value; RaisePropertyChanged("Age"); ValidateAge(); }
+        }
+
+
+        static bool _IsAgeValid;
+        public static bool IsAgeValid => _IsAgeValid;
+
+        void ValidateAge()
+        {
+            _IsAgeValid = Age > 0 && Age < 120 ? true : false;
+            RaiseStaticPropertyChanged(nameof(IsAgeValid));
+        }
+
+    }
+}
